b31ba017e91db3e33a29f503686140198a29646a
arunnidhi21 <arunnidhi21@gmail.com>
2025-12-29 16:54:31 +0000

    Added Week 1Python Programs

---
+++ Ex 1.py
@@ -0,0 +1,9 @@
+AGE=int(input("Enter ur age:"))
+HEIGHT = float(input("Enter ur height:"))
+NAME= input("enter ur name:")
+ARE_YOUASTUDENT =bool(input("R U STUDENT?? (true or false):"))
+
+print("Name:", NAME)
+print("Age:", AGE, "years")
+print("Height:", HEIGHT)
+print("Are you student :", ARE_YOUASTUDENT)
